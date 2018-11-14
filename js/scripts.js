32b7d267b6004605cee91f5df8a6710165d36585
JP_Rickabaugh <jprickabaugh@gmail.com>
2018-11-14 12:38:00 -0500

    setup

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  console.log('scripts loaded');
+
+  var myKey = config.MY_KEY;
+  var url = 'https://newsapi.org/v2/everything?q=bitcoin&from=2018-10-14&sortBy=publishedAt&apiKey=7c5d39d6dd9a4dd6be02bbc6a4b0e433' + myKey;
+
+
+});
